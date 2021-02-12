435565b13de32dd2bd259256c87610df80551a87
Michael <77156267+crowdemichael@users.noreply.github.com>
2021-02-12 20:30:26 +0700

    Update bima.go

--- bima.go
+++ bima.go
@@ -1,6 +1,13 @@
 package bima
 
+import (
+	services "github.com/crowdeco/bima/services"
+)
+
 const VERSION_MAYOR = 10000
 const VERSION_MINOR = 3000
 const VERSION_BUGFIX = 0
 const VERSION_STRING = "v1.3.0"
+
+type Repository = *services.Repository
+// list disini apa yang perlu di-export untuk minimalisir import e.g import github.com/crowdeco/bima
